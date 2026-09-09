27df1c6ad569dc3e011256d7f562e13ab11cdb7f
Umut Özkan <umutozkan123456@gmail.com>
2026-09-09 20:27:35 +0300

    changes

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from logger import log_state
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0.0
+
 
 while True:
+    dt = clock.tick(60) / 1000
+    print(dt)
     log_state()
     for event in pygame.event.get():
         pass
